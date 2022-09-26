9ff751fed0c0d5f7f1c47e56a32d8fb0b4527aeb
Adrian Gao <mingze.gao@sydney.edu.au>
2022-09-26 12:09:16 +1000

    feat(*): add tex and math support

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
-
 import react from '@astrojs/react'
 
 // https://astro.build/config
@@ -16,6 +15,29 @@ export default defineConfig({
     }
   },
   markdown: {
+    remarkPlugins: ['remark-math', 'remark-gfm'],
+    rehypePlugins: [
+      [
+        'rehype-katex',
+        {
+          // Katex plugin options
+          macros: {
+            '\\E': '\\mathbb{E}',
+            '\\C': '\\mathbb{C}',
+            '\\R': '\\mathbb{R}',
+            '\\N': '\\mathbb{N}',
+            '\\Q': '\\mathbb{Q}',
+            '\\bigO': '\\mathcal{O}',
+            '\\abs': '|#1|',
+            '\\set': '\\{ #1 \\}',
+            '\\indep': '{\\perp\\mkern-9.5mu\\perp}',
+            '\\nindep': '{\\not\\!\\perp\\!\\!\\!\\perp}',
+            '\\latex': '\\LaTeX',
+            '\\katex': '\\KaTeX'
+          }
+        }
+      ]
+    ],
     shikiConfig: {
       // Choose from Shiki's built-in themes (or add your own)
       // https://github.com/shikijs/shiki/blob/main/docs/themes.md
