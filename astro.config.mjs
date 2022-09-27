cec7f4a681a7e63d3be7bb2d9813ba05699ed62a
Adrian Gao <mingze.gao@sydney.edu.au>
2022-09-27 13:06:04 +1000

    chore(dep): change to prism for syntax highlight
    default shiki doesn't work for sas

--- astro.config.mjs
+++ astro.config.mjs
@@ -42,16 +42,6 @@ export default defineConfig({
         }
       ]
     ],
-    shikiConfig: {
-      // Choose from Shiki's built-in themes (or add your own)
-      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
-      theme: 'github-light',
-      // Add custom languages
-      // Note: Shiki has countless langs built-in, including .astro!
-      // https://github.com/shikijs/shiki/blob/main/docs/languages.md
-      langs: [],
-      // Enable word wrap to prevent horizontal scrolling
-      wrap: false
-    }
+    syntaxHighlight: 'prism'
   }
 })
