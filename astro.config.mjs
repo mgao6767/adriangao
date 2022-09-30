7096f5fcb7b83a906f4b35339f07013d1acab9c7
Adrian Gao <mingze.gao@sydney.edu.au>
2022-09-30 12:21:36 +1000

    fix(*): inline code format
    no longer use remarkPrism to transform inline code, which would change some special chars

--- astro.config.mjs
+++ astro.config.mjs
@@ -25,7 +25,13 @@ export default defineConfig({
       remarkMath,
       remarkGfm,
       remarkReadingTime,
-      [remarkPrism, { plugins: ['line-numbers'], transformInlineCode: true }]
+      [
+        remarkPrism,
+        {
+          plugins: ['line-numbers'],
+          transformInlineCode: false
+        }
+      ]
     ],
     rehypePlugins: [
       [
