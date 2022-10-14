a807d2bdca1df60ac23cd16e54c566b1868e6751
Adrian Gao <mingze.gao@sydney.edu.au>
2022-10-14 19:16:57 +1100

    refactor(*): rm dr. from site title

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Dr. Mingze Gao'
+export const SITE_TITLE = 'Mingze Gao'
 export const SITE_DESCRIPTION = "Mingze Gao's personal website!"
 export const SITE_IMAGE = '/adriangao.jpg'
 export const SITE_FAVICON = '/favicon.png'
